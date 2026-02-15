41e402a8c0c87fa179ee95a2362a28d41ac8b1da
devmostafakamal <dev.mostafa2025@gmail.com>
2026-02-15 22:02:17 +0600

    fixed

--- utlis/dbConfig.ts
+++ utlis/dbConfig.ts
@@ -12,8 +12,9 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-// Remove the if check that throws the error
-const DATABASE_URL = process.env.DATABASE_URL || "";
+// Provide a dummy connection string for build time
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgresql://user:pass@localhost:5432/db";
 
 const sql = neon(DATABASE_URL);
 export const db = drizzle(sql, { schema });
