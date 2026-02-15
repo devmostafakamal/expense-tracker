25e2738ea327dea6d430b14e2a8ac204fba6a647
devmostafakamal <dev.mostafa2025@gmail.com>
2026-02-15 21:42:01 +0600

    fixed

--- utlis/dbConfig.ts
+++ utlis/dbConfig.ts
@@ -1,10 +1,26 @@
+// import { drizzle } from "drizzle-orm/neon-http";
+// import { neon } from "@neondatabase/serverless";
+// import * as schema from "./schema";
+
+// if (!process.env.DATABASE_URL) {
+//   throw new Error("DATABASE_URL missing! Put it inside .env.local");
+// }
+
+// const sql = neon(process.env.DATABASE_URL);
+// export const db = drizzle(sql, { schema });
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL missing! Put it inside .env.local");
-}
+let db: ReturnType<typeof drizzle> | null = null;
 
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql, { schema });
+export function getDb() {
+  if (!db) {
+    if (!process.env.DATABASE_URL) {
+      throw new Error("DATABASE_URL missing! Put it inside .env.local");
+    }
+    const sql = neon(process.env.DATABASE_URL);
+    db = drizzle(sql, { schema });
+  }
+  return db;
+}
